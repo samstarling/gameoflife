4d04785222daffa8951544e2240c85e45b85a075
Sam Starling <sam.starling@gmail.com>
2012-02-22 21:24:32 +0000

    * Error in status check * Refactored alive_neighbours to separate method * Improved checking for negative grid references

--- gameoflife.rb
+++ gameoflife.rb
@@ -1,58 +1,53 @@
 class GameOfLife
   
   def initialize
-    @size = 8
+    @size = 5
     @grid = [
-      [0,0,0,0,0,0,0,0],
-      [0,0,0,0,0,0,0,0],
-      [0,1,1,0,0,0,0,0],
-      [0,0,0,0,0,0,0,0],
-      [0,0,0,0,0,0,0,0],
-      [0,0,0,0,0,0,0,0],
-      [0,0,0,0,0,0,0,0],
-      [0,0,0,0,0,0,0,0],
+      [0,0,0,0,0],
+      [0,1,0,0,0],
+      [0,1,0,1,0],
+      [0,1,1,0,0],
+      [0,0,0,0,0],
     ]
-    
     render
   end
   
   def tick
-    sleep(3)
+    sleep(1)
     next_tick = Array.new(@size) { Array.new(@size) }
     @grid.each_with_index do |line, row|
       line.each_with_index do |cell, column|
         next_tick[row][column] = cell_next_status(cell, row, column)
       end
     end
-    @grid = next_tick
+    if @grid == next_tick
+      abort("No change")
+    else
+      @grid = next_tick
+    end
   end
   
-  def cell_next_status(status, row, column)
+  def alive_neighbours(row, column)
     neighbours = [
-      [-1, -1],
-      [-1, 0],
-      [-1, 1],
-      [0, -1],
-      [0, 1],
-      [1, -1],
-      [1, 0],
-      [1, 1]
+      [-1, -1], [-1, 0], [-1, 1],
+      [0, -1], [0, 1],
+      [1, -1], [1, 0], [1, 1]
     ]
     neighbour_count = 0
     for item in neighbours
-      begin
-        if row + item[0] >= 0 and column + item[1] >= 0
-          cell = @grid[row + item[0]][column + item[1]]
-          if cell == 1
-            neighbour_count += 1
-          end
+      if (row + item[0] >= 0) and (column + item[1] >= 0)
+        if @grid[row + item[0]] and @grid[row + item[0]][column + item[1]] == 1
+          neighbour_count += 1
         end
-      rescue
-      end 
+      end
     end
-    
+    neighbour_count
+  end
+  
+  def cell_next_status(status, row, column)
+    neighbour_count = alive_neighbours(row, column)
     result = 0
-    if status = 1
+    if status == 1
       if neighbour_count == 2 or neighbour_count == 3
         result = 1  
       end
@@ -65,11 +60,10 @@ class GameOfLife
   end
   
   def render
-    print "\e[2J\e[f"
     for line in @grid
       puts line.join " "
     end
-    puts "-----"
+    puts "-" * @size * 2
   end
   
 end
